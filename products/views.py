7e083b962764cb52b9d7d948d249407edfa3c2c4
Matty-El <leachmatthewj@gmail.com>
2021-10-15 17:19:00 +0000

    Add editing products functionality

--- products/views.py
+++ products/views.py
@@ -103,12 +103,11 @@ def edit_product(request, product_id):
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Successfully updated product!')
+            messages.success(request, 'Product successfully updated!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request,
-                           ('Failed to update product. '
-                            'Please ensure the form is valid.'))
+            messages.error(request, ('Failed to update product. '
+                                     'Please ensure the form is valid.'))
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
