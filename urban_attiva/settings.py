a2ec2c536afd6655b2d64a13a571efcd6073f95a
Matty-El <leachmatthewj@gmail.com>
2021-10-25 12:29:58 +0000

    Deploy Heroku app

--- urban_attiva/settings.py
+++ urban_attiva/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-uyul14n6_s6sdite_4x#74a0(e@nt^1mq)$=pm-hh4b42-=mwa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['urban-attiva.herokuapp.com', 'localhost']
 
 # Application definition
 
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'urban_attiva.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
