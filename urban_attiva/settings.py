b54d8135d73ad52db646aa6783ea91953c8e82b5
Matty-El <leachmatthewj@gmail.com>
2021-10-25 13:24:35 +0000

    Correct AWS secret key error in settings.py

--- urban_attiva/settings.py
+++ urban_attiva/settings.py
@@ -181,7 +181,7 @@ if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'urban-attiva'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_SECERET_ACCESS_KEY')
+    AWS_SECRET_KEY_ID = os.environ.get('AWS_SECERET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
